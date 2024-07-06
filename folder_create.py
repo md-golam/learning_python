dd2c88efe4db0f9d9662ff057d545e3464b88551
Golam Kabir <navalarchkabir@gmail.com>
2024-07-06 10:29:28 +0600

    folder

--- folder_create.py
+++ folder_create.py
@@ -4,6 +4,6 @@ def folder_frm_file(file_path, target_dir)
         folder_names= file.read().split(',') #read the file content
     for i in folder_names:
         os.makedirs(i)
-file_path="D:\SAP training Files\list_of_employee.txt"
-target_dir="D:\SAP training Files\test1"
+file_path="D:\learning\learningpython\list_of_employee.txt"
+target_dir="D:\learning\learningpython\test1"
 folder_frm_file(file_path,target_dir)
